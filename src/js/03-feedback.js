c1ee9c8726682a7cedaebad1fcca54d8af44e25d
Tetiana Kharlamova <tatianakh0301@ukr.net>
2022-08-30 17:47:52 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,24 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('.feedback-form input[name = "email"]'),
+    message: document.querySelector('.feedback-form input[name = "message"]'),
+}
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.email.addEventListener('input', onEmailInput);
+refs.message.addEventListener('input', onMessageInput);
+
+function onFormSubmit() {
+    console.log('form data');
+}
+
+function onEmailInput() {
+    console.log("email", refs.email.value);   
+}
+
+function onMessageInput() {
+    console.log('message', refs.email.value);   
+}
