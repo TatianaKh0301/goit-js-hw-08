1245d3130b88ab1fb274cbd0c37e61ac02574d2d
Tetiana Kharlamova <tatianakh0301@ukr.net>
2022-08-30 15:26:06 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframeEl = document.querySelector('iframe');
+const player = new Player(iframeEl);
+const STORAGE_KEY = "videoplayer-current-time";
+
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY) || 0);
+// player.setCurrentTime(+localStorage.getItem(STORAGE_KEY));
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate(params) {
+    localStorage.setItem(STORAGE_KEY, params.seconds);
+}
