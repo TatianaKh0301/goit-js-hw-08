45b6d10a1767dd5c49f2aac4a90ffc1e68a82437
Tetiana Kharlamova <tatianakh0301@ukr.net>
2022-08-30 13:30:55 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryList = document.querySelector('.gallery');
+
+galleryList.addEventListener('click', onGalleryItemclick);
+
+const createGalleryCardMarkup = ({ preview, original, description }) => {
+    return `
+        <div class="gallery__item">
+            <a class="gallery__link" href=${original}>
+                <img class="gallery__image" src=${preview} data-source=${original} alt=${description}/>
+            </a>
+        </div>
+        `
+};
+
+
+const createGalleryCards = galleryItems.map(createGalleryCardMarkup).join(' ');
+
+galleryList.insertAdjacentHTML('beforeend', createGalleryCards);
+
+function onGalleryItemclick(event) {
+    event.preventDefault();
+    if (!event.target.classList.contains('gallery__image')) {
+        return;
+    } 
+}
+
+const gallery = new SimpleLightbox('.gallery div a', {
+    captionsData: 'alt',
+    });
 
 console.log(galleryItems);
